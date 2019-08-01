e0db400ca2ad4de4fa871f2665aae08677032917
Vladyslav Bezkrylyi <whisker1927@gmail.com>
2019-08-01 13:36:09 +0300

    fixed hw-1

--- src/costs/routes.js
+++ src/costs/routes.js
@@ -15,10 +15,11 @@ app.get('/costs/:id', (req, res) => {
 });
 
 app.get('/costs/categories', (req, res) => {
-  let oneProduct = products.find(product => {
-    return product.categories === req.query.categories;
-  });
-  res.send(oneProduct);
+  const { query } = req.query;
+  const productByCategories = products.filter(
+    product => product.categories === query,
+  );
+  res.status(200).json({ status: 'success', products: productByCategories });
 });
 
 app.use(function(req, res, next) {
